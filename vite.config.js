e96d6175a4e865d7d3c2964c30eea5735a4a4661
Rum Vannra <rumvannra002@gmail.com>
2026-08-14 21:24:12 +0700

    fix: switch to github actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
+  base: '/WanderGo_website/', // ដាក់ទីនេះត្រឹមត្រូវ (កម្រិតស្មើនឹង plugins)
   plugins: [
     tailwindcss(),
     react()
